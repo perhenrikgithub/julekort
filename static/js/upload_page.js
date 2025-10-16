ff3eec6cfaebc8edaebaf07bd6941607fd71cd91
Per Henrik Mork Viken <perhenrikmv@gmail.com>
2025-10-16 02:50:13 +0200

    add feature where if url cookie stored, html file input element is removed and button to change file is visable instead

--- static/js/upload_page.js
+++ static/js/upload_page.js
@@ -1,7 +1,8 @@
 // max file size frontend validation
 const allowedTypes = ["image/jpeg", "image/png"];
 const maxFileSize = 1 * 1024 * 1024; // 1 MB
-document.getElementById("card_image").addEventListener("change", function (e) {
+const imageInput = document.getElementById("card_image");
+imageInput.addEventListener("change", function (e) {
     if (e.target.files[0] && e.target.files[0].size > maxFileSize) {
         alert("Å nei! Filen er for stor for julenissens slede! (maks " + maxFileSize / 1024 / 1024 + "MB).");
         e.target.value = "";
@@ -25,3 +26,49 @@ cardTextArea.addEventListener("input", () => {
 });
 
 updateCardTextPreview();
+
+// removal of file input field if image_url saved as cookie
+const imageUrl = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("image_url="))
+    ?.split("=")[1];
+
+let lastRecipient = decodeURIComponent(
+    document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("recipient="))
+        ?.split("=")[1] || ""
+);
+
+// Fjern oktale escapes som \054 og erstatt med faktisk komma
+lastRecipient = lastRecipient.replace(/\\054/g, ",");
+lastRecipient = lastRecipient.replace('"', "");
+lastRecipient = lastRecipient.replace('"', "");
+
+if (lastRecipient.includes(",")) {
+    const recipients = lastRecipient.split(",");
+    lastRecipient = recipients.map((r) => r.trim()).join("\n");
+    console.log("multiple recipients", lastRecipient);
+}
+
+if (imageUrl) {
+    imageInput.style.display = "none";
+    const cardImageLabel = document.getElementById("card_image_label");
+    cardImageLabel.style.display = "none";
+    const fileInputInfo = document.getElementById("file_input_info");
+    const removeFileButton = document.getElementById("remove_file_button");
+    removeFileButton.style.display = "inline";
+    removeFileButton.addEventListener("click", () => {
+        imageInput.value = "";
+        imageInput.style.display = "block";
+        cardImageLabel.style.display = "block";
+        fileInputInfo.style.display = "none";
+        removeFileButton.style.display = "none";
+        document.getElementById("image_url").value = "";
+        // remove cookie
+        document.cookie = "image_url=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "recipient=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    });
+    fileInputInfo.innerText = "Julenissen har en kopi av bilde \ndu sendte inn sist til: \n" + lastRecipient;
+    fileInputInfo.style.display = "block";
+}
